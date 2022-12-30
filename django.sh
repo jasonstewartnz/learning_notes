57598ea66ccc1a64e409eae087aee4583cc41434
Jason Stewart <jason.stewart@gmail.com>
2022-12-30 12:56:42 -0800

    finished first Airflow tutorial

--- django.sh
+++ django.sh
@@ -1,3 +1,7 @@
+# codingforentrepreneurs tutorial repo
+# youtube: https://youtu.be/zgQ4WFGMgRM
+# https://github.com/codingforentrepreneurs/Try-Django-3.2
+
 
 # create directory
 mkdir ~/projects/try-django
@@ -6,19 +10,26 @@ cd ~/projects/try-django
 # Create vs code workspace and work in vs code terminal
 
 # create virtual env
-python3 -m venv try-django
+python3 -m venv .
 source bin/activate
 
 # pip install as per django project
 pip install Django==4.1.4
-pip freeze >> requirements.txt
+pip freeze > requirements.txt
+
+# choose python interpreter from within venv folder
+# ~/projects/try-django/bin/python3
 
 # start django project
-python -m django startproject trydjango
+python3 -m django startproject trydjango
 
 # start server
-python trydjango/manage.py runserver
+python3 trydjango/manage.py runserver
 
 # probably should also git init here
+
+
+
+
 
 
