85b9c18098f1ee23563eb7ac7e2966a4b6dc852d
Jason Stewart <jason.stewart@gmail.com>
2023-01-13 00:20:18 -0800

    Setting up git with SSH

--- git.sh
+++ git.sh
@@ -52,3 +52,34 @@ touch git.txt
   508  git add trydjango/manage.py
   509  git commit -m "Initial project commit"
   510  history
+
+  ## Connecting with SSH 
+  # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/about-ssh
+  # "You can also use an SSH key to sign commits"
+  # the ssh is proving that it it you that made those changes 
+  # (or at least someone with access to your machine - which 
+  # should only be you)
+# I prefer SSH because I cannot authenticate using a password via HTTP
+
+# For more information, see "Generating a new SSH key and adding it to the ssh-agent", "Adding a new SSH key to your GitHub account" and "About commit signature verification."
+# 
+
+# Step 1
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+ssh-keygen -t ed25519 -C "jason.stewart@gmail.com"
+# <enter> 
+# <enter>
+# <enter> (backed up existing file. Compare cat files and they are the same)
+
+# start ssh agent
+eval "$(ssh-agent -s)"
+# add key (but this was already done for me previously)
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519.pub
+# Copy and paste output to GitHub SSH key in settings
+
+cd ~/projects 
+git clone git@github.com:jasonstewartnz/gj_dbt_sandbox.git
+
+
